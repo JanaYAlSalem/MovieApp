52abd3939f0ccd4e83f82ee6318323e18bb95a79
Jana <Jana.Yousef.ruh.k@tuwaiq.edu.sa>
2022-05-25 18:02:14 +0300

    - Add first them

--- app/src/main/java/com/janayalsalem/movieapp/MainActivity.kt
+++ app/src/main/java/com/janayalsalem/movieapp/MainActivity.kt
@@ -16,28 +16,28 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MovieAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+            setContent {
+                MyApp {
+                    Content()
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(content: @Composable () -> Unit) {
+    MovieAppTheme {
+        content()
+    }
 }
+@Composable
+fun Content() {
 
+}
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MovieAppTheme {
-        Greeting("Android")
+    MyApp {
+        Content()
     }
 }
