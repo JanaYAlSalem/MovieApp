21feb46679bccc9caabd8d155acff238a9009f9c
Jana <Jana.Yousef.ruh.k@tuwaiq.edu.sa>
2022-05-27 16:31:12 +0300

    - Fixed photos

--- app/src/main/java/com/janayalsalem/movieapp/MainActivity.kt
+++ app/src/main/java/com/janayalsalem/movieapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.janayalsalem.movieapp
 
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
@@ -15,10 +14,8 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -112,15 +109,16 @@ fun MovieList(movieItem: Movie,onItemClick: (String) -> Unit = {}) {
                 .size(100.dp),
                 shape = RectangleShape,
                 elevation = 4.dp) {
-//                Image(painter = rememberImagePainter(data = movieItem.images[0],
-//                    builder = {
-//                        crossfade(true)
-//                        transformations(CircleCropTransformation())
-//                    }),
-//                    contentDescription = "Movie Poster")
+
+                Image(painter = rememberImagePainter(data = movieItem.images[0],
+                    builder = {
+                        crossfade(true)
+                        transformations(CircleCropTransformation())
+                    }),
+                    contentDescription = "Movie Poster")
 
                 // Default image
-               Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+//               Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
 
             }
             Column(modifier = Modifier.padding(4.dp)) {
